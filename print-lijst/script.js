59f465a677554cc11a54cc2377d2e5a6af3b8ca0
YounggPrince <94608647+YounggPrince@users.noreply.github.com>
2022-01-27 16:41:27 +0100

    Update script.js

---
+++ print-lijst/script.js
@@ -0,0 +1,12 @@
+var Youngboy = {sterkepunt: "sterke en veel flows", catagorie:"hiphop"};
+
+
+var Gunna = {sterkepunt: "melodie", catagorie: "hiphop/rb"};
+
+var kayFlock = {sterkepunt: "snel rappen", catagorie: "hiphop"};
+
+var lijstArtisten = [Youngboy , Gunna, Kayflock];
+for[]
+
+
+
