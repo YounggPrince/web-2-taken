797e162ca6447a5bae1c663656cbdafcccdd224d
YounggPrince <94608647+YounggPrince@users.noreply.github.com>
2022-01-28 09:51:34 +0100

    Update script.js

--- print-lijst/script.js
+++ print-lijst/script.js
@@ -1,12 +1,14 @@
-var Youngboy = {sterkepunt: "sterke en veel flows", catagorie:"hiphop"};
+var Artist1 = {name: "Youngboy" , sterkepunt: "sterke en veel flows" , catagorie: "hiphop"};
 
 
-var Gunna = {sterkepunt: "melodie", catagorie: "hiphop/rb"};
+var Artist2 = {name: "Gunna" , sterkepunt: "melodie" , catagorie: "hiphop/rb"};
 
-var kayFlock = {sterkepunt: "snel rappen", catagorie: "hiphop"};
+var Artist3 = {name: "Kflock" , sterkepunt: "snel rappen", catagorie: "hiphop"};
 
-var lijstArtisten = [Youngboy , Gunna, Kayflock];
-for[]
+var lijstArtist = [Artist1, Artist2, Artist3 ];
+for (let Artist of lijstArtist) {
+    console.log(Artist);
+    }
 
 
 
