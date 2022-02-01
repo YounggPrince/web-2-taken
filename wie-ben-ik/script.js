42f1f7ae0a92e5714c327f8f835a4894d977c5fa
YounggPrince <94608647+YounggPrince@users.noreply.github.com>
2022-02-01 16:39:45 +0100

    wie ben ik

--- wie-ben-ik/script.js
+++ wie-ben-ik/script.js
@@ -5,4 +5,14 @@
  let h1Element = document.querySelector('.h1');
  console.log(h1Element);
 
- h1Element.style.backgroundColor = 'blue';
+ h1Element.style.backgroundColor = 'red';
+
+
+ let PrinceElement = document.querySelector('#Prince');// this is identical
+ console.log(PrinceElement);
+ PrinceElement.style.backgroundColor = 'red';
+
+ document.getElementById("Prince").style.textDecoration = "underline"
+
+
+
