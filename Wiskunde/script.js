d5839f3f4527e18e2c882513e8cb73fef7e69221
YounggPrince <94608647+YounggPrince@users.noreply.github.com>
2022-02-08 16:06:56 +0100

    Update script.js

--- Wiskunde/script.js
+++ Wiskunde/script.js
@@ -5,4 +5,14 @@ let getal1 = prompt('geef het eerste getal');
 let getal2 = prompt('geef het tweede getal');
 let som = (getal1*1) + (getal2*1); // de som van de getallen hierboven
 alert (som);
+
+let isGetal1kleinerDangetal12 = getal1 < getal2;
+
+if(isGetal1kleinerDangetal12) {
+
+    aler('getal 1 is kleiner dan getal 2');
+} else{
+    // dit voeren we uit indien fout
+    alert('Getal 1 is kleiner dan getal 2');
+}
  
