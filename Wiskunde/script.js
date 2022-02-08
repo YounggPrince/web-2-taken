c34e42fc91707f73aeebb7fd930ea0dbc51434a6
YounggPrince <94608647+YounggPrince@users.noreply.github.com>
2022-02-08 14:23:00 +0100

    update web 2 taken

---
+++ Wiskunde/script.js
@@ -0,0 +1,8 @@
+//alert gaat een popup tonen met een bericht
+//hier is het bericht de som vanhet geval 12 + 13
+
+let getal1 = prompt('geef het eerste getal');
+let getal2 = prompt('geef het tweede getal');
+let som = (getal1*1) + (getal2*1); // de som van de getallen hierboven
+alert (som);
+ 
