033ed65fb896d0252a683d372f01c64c79077106
YounggPrince <94608647+YounggPrince@users.noreply.github.com>
2022-02-15 15:59:56 +0100

    floor

--- random-floor/script.js
+++ random-floor/script.js
@@ -9,13 +9,13 @@ console.log('geheel getal afgerond ' + geheelGetalAfgerond)
 
 let randomGetal = Math.random();
 // Math.random() maakt een willekeurig getal tusen 0 en 1 tot 17 cijfers na de komma
-console.log('random nummer tussen 0 en 1 ' + randomGetal)
+console.log('random nummer tussen 0 en 255 ' + randomGetal)
 
 // om een willekeurige waarde te verkrijgen tussen 0 en een maximum waarde
 // moet je Math.random() vermenigvuldigen met de maximum waarde
 // dus bv om een getal tussen 0 en 10 te krijgen, doe je Math.random() * 10
-let randomGetal10 = Math.random() * 10;
-console.log('random nummer tussen 0 en 10 ' +  randomGetal10)
+let randomGetal10 = Math.random() * 255;
+console.log('random nummer tussen 0 en 255 ' +  randomGetal10)
 
 // hier gaan we het random getal tussen 0 en 10 afronden
 let randomGetal10Afgerond = Math.floor(randomGetal10);
@@ -26,4 +26,5 @@ console.log('geheel getal afgerond ' + randomGetal10Afgerond)
 // en dan zie je hoe we dat getal afronden (cijfers na de komma weghalen)
 // 1. een willekeurig getal uitprinten die tussen 0 en 255 is > gebruik Math.random()
 // 2. en dan van dat getal, alle cijfers na de komma wegdoen > gebruik Math.floor()
+
 
