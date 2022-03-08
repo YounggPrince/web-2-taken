1b53c37e95317a82ca158748de85bae5dfbf6b7a
YounggPrince <94608647+YounggPrince@users.noreply.github.com>
2022-03-08 15:11:19 +0100

    functie

---
+++ functie/script.js
@@ -0,0 +1,13 @@
+var Artist1 = {name: "Youngboy" , sterkepunt: "sterke en veel flows" , catagorie: "hiphop"};
+var Artist2 = {name: "Gunna" , sterkepunt: "melodie" , catagorie: "hiphop/rb"};
+var Artist3 = {name: "Kflock" , sterkepunt: "snel rappen", catagorie: "hiphop"};
+var lijstArtist = [Artist1, Artist2, Artist3 ];
+
+  console.log(lijstArtist.length);
+for (let i = 0; i <lijstArtist; i++) {
+    console.log(lijstArtist[i]); 
+}
+
+
+
+  
