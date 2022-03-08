e197d21a0c9e2a9df6b0d8cc3878819c512bf8e1
YounggPrince <94608647+YounggPrince@users.noreply.github.com>
2022-03-08 16:21:54 +0100

    functie

--- functie/script.js
+++ functie/script.js
@@ -1,12 +1,22 @@
-var Artist1 = {name: "Youngboy" , sterkepunt: "sterke en veel flows" , catagorie: "hiphop"};
-var Artist2 = {name: "Gunna" , sterkepunt: "melodie" , catagorie: "hiphop/rb"};
-var Artist3 = {name: "Kflock" , sterkepunt: "snel rappen", catagorie: "hiphop"};
+var Artist1 = {name: "Youngboy" , sterkpunt: "sterke en veel flows" , catagorie: "hiphop"};
+var Artist2 = {name: "Gunna" , sterkpunt: "melodie" , catagorie: "hiphop/rb"};
+var Artist3 = {name: "Kflock" , sterkpunt: "snel rappen", catagorie: "hiphop"};
 var lijstArtist = [Artist1, Artist2, Artist3 ];
 
   console.log(lijstArtist.length);
-for (let i = 0; i <lijstArtist; i++) {
-    console.log(lijstArtist[i]); 
+
+for (let i = 0; i <lijstArtist.length; i++) {
+    Artistprinten(lijstArtist[i]);
 }
+function Artistprinten (parameter){
+console.log(parameter.name + ' ' + parameter.sterkpunt + ' ' + parameter.catagorie)
+}
+
+
+
+
+
+  
 
 
 
